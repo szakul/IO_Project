3713510e68549826373412849078ee51afeeccb2
Łukasz Kępka (lukakep909) <lukakep909@student.polsl.pl>
2017-02-08 13:04:39 +0100

    Uploaded project files

---
+++ SportCenterManager/SportCenterManager/MainWindow.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SportCenterManager
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageAddEvent = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxEventDescription = new System.Windows.Forms.TextBox();
+            this.textBoxEventName = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPageAddEvent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageAddEvent);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(286, 221);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageAddEvent
+            // 
+            this.tabPageAddEvent.Controls.Add(this.button1);
+            this.tabPageAddEvent.Controls.Add(this.label2);
+            this.tabPageAddEvent.Controls.Add(this.label1);
+            this.tabPageAddEvent.Controls.Add(this.textBoxEventDescription);
+            this.tabPageAddEvent.Controls.Add(this.textBoxEventName);
+            this.tabPageAddEvent.Location = new System.Drawing.Point(4, 22);
+            this.tabPageAddEvent.Name = "tabPageAddEvent";
+            this.tabPageAddEvent.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAddEvent.Size = new System.Drawing.Size(278, 195);
+            this.tabPageAddEvent.TabIndex = 0;
+            this.tabPageAddEvent.Text = "Add event";
+            this.tabPageAddEvent.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add event";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Event description:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Event name:";
+            // 
+            // textBoxEventDescription
+            // 
+            this.textBoxEventDescription.Location = new System.Drawing.Point(3, 58);
+            this.textBoxEventDescription.Multiline = true;
+            this.textBoxEventDescription.Name = "textBoxEventDescription";
+            this.textBoxEventDescription.Size = new System.Drawing.Size(269, 101);
+            this.textBoxEventDescription.TabIndex = 1;
+            // 
+            // textBoxEventName
+            // 
+            this.textBoxEventName.Location = new System.Drawing.Point(3, 19);
+            this.textBoxEventName.Name = "textBoxEventName";
+            this.textBoxEventName.Size = new System.Drawing.Size(269, 20);
+            this.textBoxEventName.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(278, 195);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(278, 195);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "You are logged as Admin with ID = 1";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 260);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageAddEvent.ResumeLayout(false);
+            this.tabPageAddEvent.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageAddEvent;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxEventDescription;
+        private System.Windows.Forms.TextBox textBoxEventName;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ SportCenterManager/SportCenterManager/MainWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SportCenterManager
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    int currentAccountId = 1; //Admin with this id MUST exist in db.
+        //    try
+        //    {
+        //        events newEvent = new events();
+        //        //newEvent.ID = ; u dont have to specify it. It'll be incremented automaticly
+        //        newEvent.NAME = textBoxEventName.Text;
+        //        newEvent.DESCRIPTION = textBoxEventDescription.Text;
+        //        newEvent.CREATOR_ID = currentAccountId;
+        //        using (var context = new osrodeksportowydbEntities2())
+        //        {
+        //            context.events.Add(newEvent);
+        //            context.SaveChanges();
+        //        }
+        //    }catch(DbUpdateException ex)
+        //    {
+        //        //shuold be checked for nulls. And there is probably a better way to get DB exception.
+        //        MessageBox.Show(ex.Message + "\n" + ex.InnerException.InnerException.Message);
+        //    }
+        //}
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var context = new DatabaseConnection())
+            {
+                var t = context.trainings.Find(1);
+                textBoxEventName.Text = t.ID + " " + t.NAME + " " + t.DESCRIPTION;
+            }
+        }
+
+
+    }
+}
