149464eb19ebcc2ffb650b66735b7e29c5705934
Marcin Pszczółka <Marcin Pszczka>
2017-02-10 12:18:32 +0100

    admin window with correct db model

--- SportCenterManager/SportCenterManager/MainWindow.cs
+++ SportCenterManager/SportCenterManager/MainWindow.cs
@@ -16,39 +16,54 @@ namespace SportCenterManager
         public MainWindow()
         {
             InitializeComponent();
+            using (var context = new DatabaseConnection())
+            {
+                var reservableFacilities = (from f in context.facilities
+                            where f.RESERVABLE == true
+                            select new {id = f.ID, name = f.NAME}).ToArray();
+                comboBoxEventFacility.Items.AddRange(reservableFacilities);
+            }
         }
-
-        //private void button1_Click(object sender, EventArgs e)
-        //{
-        //    int currentAccountId = 1; //Admin with this id MUST exist in db.
-        //    try
-        //    {
-        //        events newEvent = new events();
-        //        //newEvent.ID = ; u dont have to specify it. It'll be incremented automaticly
-        //        newEvent.NAME = textBoxEventName.Text;
-        //        newEvent.DESCRIPTION = textBoxEventDescription.Text;
-        //        newEvent.CREATOR_ID = currentAccountId;
-        //        using (var context = new osrodeksportowydbEntities2())
-        //        {
-        //            context.events.Add(newEvent);
-        //            context.SaveChanges();
-        //        }
-        //    }catch(DbUpdateException ex)
-        //    {
-        //        //shuold be checked for nulls. And there is probably a better way to get DB exception.
-        //        MessageBox.Show(ex.Message + "\n" + ex.InnerException.InnerException.Message);
-        //    }
-        //}
-
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (var context = new DatabaseConnection())
+            int currentAccountId = 1; //Admin with this id MUST exist in db.
+            try
             {
-                var t = context.trainings.Find(1);
-                textBoxEventName.Text = t.ID + " " + t.NAME + " " + t.DESCRIPTION;
+                events newEvent = new events();
+                reservations newReservation = new reservations();
+                //newEvent.ID = ; u dont have to specify it. It'll be incremented automaticly
+                newEvent.NAME = textBoxEventName.Text;
+                newEvent.DESCRIPTION = textBoxEventDescription.Text;
+                newEvent.CREATOR_ID = currentAccountId;
+
+                newReservation.CREATOR_ID = currentAccountId;
+                newReservation.TRAINING_ID = null;
+                //newReservation.FACILITY_ID = 
+                //newReservation.EVENT_ID = 
+
+                using (var context = new DatabaseConnection())
+                {
+                    context.events.Add(newEvent);
+                    context.SaveChanges();
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                //shuold be checked for nulls. And there is probably a better way to get DB exception.
+                MessageBox.Show(ex.Message + "\n" + ex.InnerException.InnerException.Message);
             }
         }
+
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    using (var context = new DatabaseConnection())
+        //    {
+        //        var t = context.trainings.Find(1);
+        //        textBoxEventName.Text = t.ID + " " + t.NAME + " " + t.DESCRIPTION;
+        //    }
+        //}
 
 
     }
