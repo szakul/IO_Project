c21af9e64786605daaebfdcd31cdf7aab531458c
Łukasz Kępka (lukakep909) <lukakep909@student.polsl.pl>
2017-02-14 11:03:12 +0100

    DataGrid row update 2

--- SportCenterManager/SportCenterManager/Model/DataGridRowsBuilder.cs
+++ SportCenterManager/SportCenterManager/Model/DataGridRowsBuilder.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace SportCenterManager.Model
 {
-    class DataGridRowsBuilder
+    public class DataGridRowsBuilder
     {
+        public virtual IList<DataGridRow> LoadFromDatabase(DatabaseConnection context, int? creatorId = null)
+        {
+            IList<DataGridRow> rowsList = new List<DataGridRow>();
+            var reservationList = context.reservations.Where(i => (creatorId != null) ? i.CREATOR_ID == creatorId : true).ToList();
+            foreach (reservations reservation in reservationList)
+            {
+                trainings training = context.trainings.Where(i => i.ID == reservation.TRAINING_ID).FirstOrDefault();
+                facilities facility = context.facilities.Where(i => i.ID == reservation.FACILITY_ID).FirstOrDefault();
+                DataGridRow row = new DataGridRow(id: reservation.ID, facilityName: facility?.NAME, trainingName: training?.NAME, accepted: reservation.ACCEPTED, start: reservation.START, end: reservation.END);
+                rowsList.Add(row);
+            }
+            return rowsList;
+        }
     }
 }
